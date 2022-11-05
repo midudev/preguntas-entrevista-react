0f4e7bbe1f471a7082c6afcf9bed7d26305f76af
Irwing Naranjo <khalisser@gmail.com>
2022-11-04 21:37:13 -0300

    Add button read

--- app/[post]/page.js
+++ app/[post]/page.js
@@ -2,6 +2,7 @@ import { readJSON } from 'fs-extra'
 import path from 'node:path'
 
 import { Pill } from '../components/Pill.jsx'
+import { ButtonRead } from '../components/ButtonRead.jsx'
 
 const jsonDirectory = path.join(process.cwd(), 'dist')
 
@@ -19,7 +20,10 @@ export default async function Post ({ params }) {
     <>
       <header className='relative'>
         <h1 className='pb-8 text-4xl font-bold text-blue-900 font-grotesk' id='content'>{title}</h1>
-        <Pill level={level} />
+        <div className='absolute top-3 right-1 flex flex-row gap-2'>
+          <Pill level={level} />
+          <ButtonRead title={title} />
+        </div>
       </header>
       <article className='prose max-w-none pb-4 [&>h1]:text-3xl [&>h1]:font-bold [&>h1]:text-blue-900 [&>h1]:pb-8 [&>p]:pb-6 [&>p]:text-xl [&>p>strong]:bg-yellow-50 [&_a]:text-blue-700 [&_a:hover]:underline [&>ul>li]:list-disc [&>ul]:text-xl [&>ul]:text-blue-900 [&>ul]:pb-4 [&>ul>li]:list-inside [&>pre]:rounded [&>pre]:text-white [&>pre]:mb-8 [&>pre]:p-4 [&>pre]:bg-slate-800' dangerouslySetInnerHTML={{ __html: render }} />
     </>
