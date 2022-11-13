96dec20e4f8ce4dbecd03f97d8120a4b52e859c5
Miguel Ángel Durán <miduga@gmail.com>
2022-11-13 12:06:03 +0100

    Move to static content

--- app/[post]/page.js
+++ app/[post]/page.js
@@ -7,15 +7,21 @@ import { Title } from './Title.jsx'
 
 const jsonDirectory = path.join(process.cwd(), 'dist')
 
+export async function generateStaticParams () {
+  const index = path.join(jsonDirectory, 'index.json')
+  const posts = await readJSON(index)
+
+  return posts.map(post => ({ post: post.id }))
+}
+
+const fetchPost = async (slug) => {
+  const { content, level, title } = await readJSON(`${jsonDirectory}/${slug}.json`)
+  return { content, level, title }
+}
+
 export default async function Post ({ params }) {
   const { post } = params
-
-  const { content, level, title } = await readJSON(`${jsonDirectory}/${post}.json`)
-
-  const render = content
-    .replace('<h4 ', '<h1 ')
-    .replace('</h4', '</h1')
-    .replace('<hr>', '')
+  const { content, level, title } = await fetchPost(post)
 
   return (
     <>
@@ -27,7 +33,7 @@ export default async function Post ({ params }) {
         </div>
         <h1 className='pb-8 text-3xl font-bold leading-snug text-blue-900 md:text-4xl' id='content'>{title}</h1>
       </header>
-      <article className='prose max-w-none pb-4 [&>h1]:text-3xl [&>h1]:font-bold [&>h1]:text-blue-900 [&>h1]:pb-8 [&>p]:pb-6 [&>p]:text-lg md:[&>p]:text-xl [&>p>strong]:bg-yellow-50 [&_a]:text-blue-700 [&_a:hover]:underline [&>ul>li]:list-disc [&>ul]:text-lg md:[&>ul]:text-xl [&>ul]:text-blue-900 [&>ul]:pb-4 [&>ul>li]:ml-5 [&>ul]:space-y-3 [&>pre]:overflow-x-auto  [&>pre]:rounded-xl [&>pre]:text-white [&>pre]:mb-8 [&>pre]:p-8 [&>pre]:bg-slate-800' dangerouslySetInnerHTML={{ __html: render }} />
+      <article className='prose max-w-none pb-4 [&>h1]:text-3xl [&>h1]:font-bold [&>h1]:text-blue-900 [&>h1]:pb-8 [&>p]:pb-6 [&>p]:text-lg md:[&>p]:text-xl [&>p>strong]:bg-yellow-50 [&_a]:text-blue-700 [&_a:hover]:underline [&>ul>li]:list-disc [&>ul]:text-lg md:[&>ul]:text-xl [&>ul]:text-blue-900 [&>ul]:pb-4 [&>ul>li]:ml-5 [&>ul]:space-y-3 [&>pre]:overflow-x-auto  [&>pre]:rounded-xl [&>pre]:text-white [&>pre]:mb-8 [&>pre]:p-8 [&>pre]:bg-slate-800' dangerouslySetInnerHTML={{ __html: content }} />
     </>
   )
 }
