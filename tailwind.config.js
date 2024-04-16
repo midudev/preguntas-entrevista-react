6adef0174b327b6378fb1199ad8cc85b4f9d4bb2
Miguel Ángel Durán <miduga@gmail.com>
2024-04-16 10:54:55 +0200

    Format file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
+  darkMode: 'class',
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './app/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
-      colors:{
-        primary:"#000000",
-        secondry:"#121212"
+      colors: {
+        primary: '#000000',
+        secondry: '#121212'
       },
       backgroundImage: {
         'blue-gradient-radial': 'radial-gradient(circle,#0141ff 0,rgba(161,252,70,0) 71%)'
@@ -19,7 +19,7 @@ module.exports = {
       },
       boxShadow: {
         box: '0 0 0 1px rgb(0 0 0 / 7%), 0 2px 4px rgb(0 0 0 / 5%), 0 12px 24px rgb(0 0 0 / 5%)',
-        darkbox: '0 0 0 1px rgb(91 91 91 / 7%), 0 2px 4px rgb(91 91 91 / 5%), 0 12px 24px rgb(91 91 91 / 5%)'
+        darkbox: '0 0 0 1px rgb(255 255 255 / 7%), 0 2px 4px rgb(255 255 255 / 5%), 0 12px 24px rgb(255 255 255 / 5%)'
       }
     }
   },
