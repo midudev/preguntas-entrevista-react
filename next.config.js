b0168a7c462d5b69e54087cc6b2ac4c0d8e3a278
Miguel Ángel Durán <miduga@gmail.com>
2024-04-16 10:45:10 +0200

    Remove not needed config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
