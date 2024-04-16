261f1afb63a3a917a7e03cdb888a33ea5e364524
Miguel Ángel Durán <miduga@gmail.com>
2024-04-16 11:08:43 +0200

    Ignore Eslint errors

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
+  }
 }
 
 module.exports = nextConfig
