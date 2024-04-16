a9772a7e7c1b5e849b1bad5632dc2c99d5b9ae16
Miguel Ángel Durán <miduga@gmail.com>
2024-04-16 10:55:00 +0200

    Use next/font

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
 import './globals.css'
-import { Space_Grotesk as SpaceGrotesk } from '@next/font/google'
+import { Space_Grotesk as SpaceGrotesk } from 'next/font/google'
 import { Header } from './components/Header.jsx'
 import { Footer } from './components/Footer.jsx'
 import { BuyBook } from './components/BuyBook.jsx'
 import { Head } from './components/Head'
-import { ThemeContextProvider } from "../context/ThemeContext";
-import ThemeProvider from "../provider/ThemeProvider";
+import { ThemeContextProvider } from '../context/ThemeContext'
+import ThemeProvider from '../provider/ThemeProvider'
 
 const spaceGrotesk = SpaceGrotesk({ weight: ['400', '700'], subsets: ['latin'] })
 
@@ -15,7 +15,7 @@ const fetchGitHubStars = () => {
     .then(response => response.stargazers_count)
 }
 
-export default async function RootLayout({ children }) {
+export default async function RootLayout ({ children }) {
   const stars = await fetchGitHubStars()
 
   return (
@@ -27,7 +27,7 @@ export default async function RootLayout({ children }) {
       <body className={`${spaceGrotesk.className} overscroll-none`}>
         <ThemeContextProvider>
           <ThemeProvider>
-            <div className="bg-white text-black dark:bg-secondry dark:text-white">
+            <div className='bg-white text-black dark:bg-secondry dark:text-white'>
               <div aria-hidden='true' className='absolute inset-0 z-0 overflow-hidden pointer-events-none'>
                 <div className='absolute top-0 scale-150 rounded-full bg-blue-gradient-radial w-96 h-96 left-14 opacity-20' />
               </div>
