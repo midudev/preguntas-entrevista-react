f6eb561bbc518eefbb67d550a041eab6dfa2e5b9
Miguel Ángel Durán <miduga@gmail.com>
2024-04-16 10:59:17 +0200

    Fix URL

--- utils/posts.js
+++ utils/posts.js
@@ -1,9 +1,13 @@
-const res = await fetch('http://localhost:3000/content/index.json')
+const IS_PROD = process.env.NODE_ENV === 'production'
+const BASE_URL = IS_PROD ? 'https://reactjs.wiki/' : 'http://localhost:3000'
+
+const res = await fetch(`${BASE_URL}/content/index.json`)
 const posts = await res.json()
 
 export const fetchPost = async (slug) => {
-  const res = await fetch(`http://localhost:3000/content/${slug}.json`)
+  const res = await fetch(`${BASE_URL}/content/${slug}.json`)
   const { content, level, title, id } = await res.json()
+  console.log(content, level, title, id)
   const currentIndex = posts.findIndex(post => post.id === id)
   const prev = currentIndex > 0 ? posts[currentIndex - 1] : null
   const next = currentIndex < posts.length ? posts[currentIndex + 1] : null
