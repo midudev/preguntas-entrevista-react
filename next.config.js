9a1fc3aaacce54bfa3b8ceb21b3b337961642c24
midudev <miduga@gmail.com>
2025-05-06 10:33:23 +0200

    Add comma dangle

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true
-  }
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
