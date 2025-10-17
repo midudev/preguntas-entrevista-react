79a746bc031668d8fa551c6c54b87f5c1237e331
midudev <miduga@gmail.com>
2025-10-17 20:31:56 +0200

    refactor: remove unnecessary React import from ThemeProvider

--- provider/ThemeProvider.jsx
+++ provider/ThemeProvider.jsx
@@ -1,6 +1,6 @@
 'use client'
 import { ThemeContext } from '../context/ThemeContext'
-import React, { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 const ThemeProvider = ({ children }) => {
   const { theme } = useContext(ThemeContext)
