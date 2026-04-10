059ce9e99367a01ee772935d3ed76668c748fb01
Miguel Ángel Durán (midudev) <miduga@gmail.com>
2026-04-10 19:10:39 +0200

    feat: elimina consola de depuración en fetchPost

--- utils/posts.js
+++ utils/posts.js
@@ -8,7 +8,6 @@ export const fetchPost = async slug => {
   const posts = await readIndex()
 
   const post = await import(`../public/content/${slug}.json`)
-  console.log(post)
   const { content, level, title, id } = post
 
   const currentIndex = posts.findIndex(post => post.id === id)
