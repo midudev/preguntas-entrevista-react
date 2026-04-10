0c0a567590bf5a4c0564fe3a79999317782b3f0f
Miguel Ángel Durán (midudev) <miduga@gmail.com>
2026-04-10 19:01:04 +0200

    chore: migrate lint setup to oxlint

--- eslint.config.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports = [
-  { ignores: ['.next/**'] },
-  ...require('neostandard')({
-    noStyle: true,
-  }),
-]
--- next.config.js
+++ next.config.js
@@ -6,11 +6,6 @@ const nextConfig = {
   turbopack: {
     root: path.join(__dirname, '.'),
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 }
 
 module.exports = nextConfig
